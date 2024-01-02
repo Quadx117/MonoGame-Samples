9076eb1f9cd65e76049d67ed8a5e452a0b3e1116
Quadx117 <Quadx117@users.noreply.github.com>
2024-01-02 17:44:59 -0500

    Miscellaneous - Add NinePatch project

---
+++ Miscellaneous/UI/NinePatch/NinePatchDemo.cs
@@ -0,0 +1,58 @@
+﻿namespace NinePatch;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using NinePatch.Managers;
+
+public class NinePatchDemo : Game
+{
+    private readonly GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private GameManager _gameManager;
+
+    public NinePatchDemo()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        _graphics.PreferredBackBufferWidth = 1024;
+        _graphics.PreferredBackBufferHeight = 768;
+        _graphics.ApplyChanges();
+
+        _gameManager = new GameManager(Content);
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+        {
+            Exit();
+        }
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+
+        _spriteBatch.Begin();
+        _gameManager.Draw(_spriteBatch);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
